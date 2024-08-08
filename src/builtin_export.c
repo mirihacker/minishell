c94b08f8ed34b6f4e43e515f57b6a76b21040b71
Siria Lizeth Miranda Rendon <smiranda@3-F-14.42heilbronn.de>
2024-08-08 22:01:19 +0200

    under construction

---
+++ src/builtin_export.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_export.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smiranda <smiranda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/08 19:31:17 by smiranda          #+#    #+#             */
+/*   Updated: 2024/08/08 22:00:38 by smiranda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/minishell.h"
+
+int ft_export(char **argv)
+{
+    char *name;
+    char *value;
+
+    if (ft_strchr(argv, '='))
+    {
+        name = ft_substr(argv, 0, ft_strchr(argv, '=') - argv);
+        value = ft_strdup(ft_strchr(argv, '=') + 1);
+        if (!name || !value)
+            return (EXIT_FAILURE);
+        if (ft_getenv(name)) //to do : check if the name is valid
+            ft_setenv(name, value); //to do : check if the value is valid
+        else
+            ft_putenv(argv); //to do : check if the name is valid
+        free(name);
+        free(value);
+    }
+    else
+    {
+        if (ft_getenv(argv))
+            ft_setenv(argv, "");
+        else
+            ft_putenv(argv); //to do : add env to the list
+    }
+}
+
+int builtin_export(char **argv)
+{
+    int i;
+
+    i = 1;
+    if (!argv[i])
+        return(builtin_env(argv));
+    if (is_option(argv[i], "export"))
+        return(EXIT_FAILURE);
+    while (argv[i])
+    {
+        ft_export(argv[i]);
+        i++;
+    }
+    return(EXIT_SUCCESS);   
+}
+
