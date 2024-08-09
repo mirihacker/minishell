3458911d7819b1cc6ec7f4423d32926429ce6418
Siria Lizeth Miranda Rendon <smiranda@2-F-4.42heilbronn.de>
2024-08-09 18:12:24 +0200

    update:)

--- src/builtin_export.c
+++ src/builtin_export.c
@@ -6,37 +6,205 @@
 /*   By: smiranda <smiranda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 19:31:17 by smiranda          #+#    #+#             */
-/*   Updated: 2024/08/08 22:05:09 by smiranda         ###   ########.fr       */
+/*   Updated: 2024/08/09 18:00:31 by smiranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-int ft_export(char **argv)
+t_list *search_env(char *name)
+{
+    t_list *ptr;
+    char *env_var;
+    int lkey;
+    t_mini *mini1;
+
+    mini1 = mini();
+    lkey = ft_strlen(name);
+    ptr = mini1->env_list;
+    while (ptr)
+    {
+        env_var = ptr->content;
+        if (!ft_strncmp(env_var, name, lkey) && env_var[lkey] == '=')
+            return (ptr);
+        ptr = ptr->next;
+    }
+    return (NULL);
+}
+
+int set_env(t_list *ptr, char *name, char *value)
+{
+    char *new_env_var;
+    char *temp;
+
+    temp = ft_strjoin(name, "=");
+    if (!temp)
+        return (EXIT_FAILURE);
+    new_env_var = ft_strjoin(temp, value);
+    free(temp);
+    if (!new_env_var)
+        return (EXIT_FAILURE);
+    free(ptr->content);
+    ptr->content = new_env_var;
+    return (EXIT_SUCCESS);
+}
+int put_env(char *name, char* value)
+{
+    t_mini *mini;
+    char *new_env_var;
+    char *temp;
+
+    mini = get_mini();
+    temp = ft_strjoin(name, "=");
+    if (!temp)
+        return (EXIT_FAILURE);
+    new_env_var = ft_strjoin(temp, value);
+    free(temp);
+    if (!new_env_var)
+        return (EXIT_FAILURE);
+    ft_listadd_back(&(mini->env_list), ft_lstnew(new_env_var));
+    return (EXIT_SUCCESS);
+}
+
+int empty_env(char **argv)
+{
+    t_list *env_entry;
+    int add_empty_env;
+
+    env_entry = search_env(argv);
+    if (!env_entry)
+    {
+        add_empty_env = put_env(argv, "");
+        if (add_empty_env == EXIT_FAILURE)
+            return (EXIT_FAILURE);
+    }
+    return (EXIT_SUCCESS);
+}
+
+int add_env(char *argv)
 {
     char *name;
     char *value;
+    t_list *env_entry;
+    int flag;
+
+    name = ft_substr(argv, 0, ft_strchr(argv, '=') - argv);
+    value = ft_strdup(ft_strchr(argv, '=') + 1);
+    if (!name || !value)
+            return (EXIT_FAILURE);
+    env_entry = search_env(name);
+    if (env_entry) //check if the name is already an env var
+        flag = set_env(env_entry, name, value); //add the new value to the env var
+    else
+        flag = put_env(name, value); //if the name is not an env var, add it to the list
+    free(name);
+    free(value);
+    if (flag == EXIT_FAILURE)
+        return (EXIT_FAILURE);
+    return (EXIT_SUCCESS);
+}
+
+int handle_export(char *argv)
+{
+    char *name;
+    char *value;
+    t_list *env_entry;
+    int flag_em;
 
     if (ft_strchr(argv, '='))
-    {
-        name = ft_substr(argv, 0, ft_strchr(argv, '=') - argv);
-        value = ft_strdup(ft_strchr(argv, '=') + 1);
-        if (!name || !value)
-            return (EXIT_FAILURE);
-        if (ft_getenv(name)) //to do : check if the name is valid
-            ft_setenv(name, value); //to do : check if the value is valid
-        else
-            ft_putenv(argv); //to do : check if the name is valid
-        free(name);
-        free(value);
-    }
+        flag_em = add_env(argv);
     else
+        flag_em = empty_env(argv);
+    if (flag_em == EXIT_FAILURE)
+        return (EXIT_FAILURE);
+    return (EXIT_SUCCESS);
+}
+
+int ft_lstclear(t_list **lst, void (*del)(void *))
+{
+    t_list *temp;
+
+    if (!lst || !del)
+        return (EXIT_FAILURE);
+    while (*lst)
     {
-        if (ft_getenv(argv)) //to do
-            ft_setenv(argv, "");
-        else
-            ft_putenv(argv); //to do : add env to the list
+        temp = *lst;
+        *lst = (*lst)->next;
+        del(temp->content);
+        free(temp);
     }
+    return (EXIT_SUCCESS);
+}
+
+t_list *ft_lstsort(t_list *list)
+{
+    t_list *ptr;
+    t_list *next;
+    char *temp;
+
+    ptr = list;
+    while (ptr)
+    {
+        next = ptr->next;
+        while (next)
+        {
+            if (ft_strcmp(ptr->content, next->content) > 0)
+            {
+                temp = ptr->content;
+                ptr->content = next->content;
+                next->content = temp;
+            }
+            next = next->next;
+        }
+        ptr = ptr->next;
+    }
+    return (list);
+}
+
+t_list *copy_list(t_list *list)
+{
+    t_list *copy;
+    t_list *new;
+
+    copy = NULL;
+    while (list)
+    {
+        new = ft_lstnew(ft_strdup(list->content));
+        if (!new)
+        {
+            ft_lstclear(&copy, free);
+            return (NULL);
+        }
+        ft_lstadd_back(&copy, new);
+        list = list->next;
+    }
+    return (copy);
+}
+
+int print_list(t_list *list)
+{
+    t_list *ptr;
+
+    ptr = list;
+    while (ptr)
+    {
+        ft_putendl_fd(ptr->content, STDOUT_FILENO);
+        ptr = ptr->next;
+    }
+    ft_lstclear(&list, free);
+    return (EXIT_SUCCESS);
+}
+
+int print_sort_env(void)
+{
+    t_mini *mini;
+    t_list *copy;
+
+    mini = get_mini();
+    copy = copy_list(mini->env_list);
+    if (!copy)
+        return (EXIT_FAILURE);
+    return (print_list(ft_lstsort(copy)));
 }
 
 int builtin_export(char **argv)
@@ -45,14 +213,15 @@ int builtin_export(char **argv)
 
     i = 1;
     if (!argv[i])
-        return(builtin_env(argv));
+        return(print_sort_env());
     if (is_option(argv[i], "export"))
         return(EXIT_FAILURE);
     while (argv[i])
     {
-        ft_export(argv[i]);
+        if (handle_export(argv[i]) == EXIT_FAILURE)
+            return(EXIT_FAILURE);
         i++;
     }
-    return(EXIT_SUCCESS);   
+    return(EXIT_SUCCESS);  
 }
 
--- src/envvar_handler.c
+++ src/envvar_handler.c
@@ -6,7 +6,7 @@
 /*   By: smiranda <smiranda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 15:20:50 by smiranda          #+#    #+#             */
-/*   Updated: 2024/07/25 13:49:31 by smiranda         ###   ########.fr       */
+/*   Updated: 2024/08/09 13:40:48 by smiranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,19 +37,24 @@ void	initialize_env(char **envp) // tb added to main
 // 	return (0);
 // }
 
-char	*get_env(char *key)
+char	*get_var_val(char *key)
 {
 	t_list	*ptr;
 	int		lkey;
 	char	*env_var;
 
+	
 	lkey = ft_strlen(key);
 	ptr = mini->env_list;
+	// Check if key is a special parameter, typically holds the exit code of the last command
+	// if key is equal to "?" then return the exit code
 	if (!ft_strncmp(key, "?", 2))
 		return (ft_itoa(mini->exit_code));
+	// Iterate through the linked list of environment variables
 	while (ptr)
 	{
 		env_var = ptr->content;
+		// Check if the key is equal to the environment variable and the next char is =
 		if (!ft_strncmp(env_var, key, lkey) && env_var[lkey] == '=')
 			return (ft_strchr(ptr->content, '=') + 1);
 		ptr = ptr->next;
@@ -57,18 +62,23 @@ char	*get_env(char *key)
 	return (NULL);
 }
 
-char	*get_substitution(char **start)
+char	*get_var_name(char **start)
 {
 	char	*end;
 	char	*env;
 
+	// Move the end pointer to the next character after the start pointer (pass the '$)
 	end = ++(*start);
+	// Check if the first character is a digit or special char, if so move the end pointer to the next char
 	if (ft_isdigit(*end) || ft_strchr("!#$*-?@_", *end))
 		end++;
 	else
+		// If the first character is a letter, underscore, or digit, move the end pointer to the next character (until the end)
 		while (*end && (ft_isalnum(*end) || *end == '_'))
 			end++;
+	// Create a substring from the start pointer to the end of var name
 	env = ft_substr(*start, 0, end - *start);
+	// Move the start pointer to the end of var name
 	*start = --end;
 	return (env);
 }
@@ -79,8 +89,8 @@ char	*envvar_handler(char **start, char *str)
 	char	*tmp;
 	char	*env;
 
-	env = get_substitution(start);
-	value = get_env(env);
+	env = get_var_name(start);
+	value = get_var_val(env);
 	if (!value)
 		value = "";
 	if (!str)
