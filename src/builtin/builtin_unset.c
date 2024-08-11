22e848b39b41fd52776fff7d449b5a9981f061d5
Siria <siria_miren@icloud.com>
2024-08-11 17:39:19 +0200

    add unset with content

---
+++ src/builtin/builtin_unset.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_unset.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smiranda <smiranda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/11 15:31:02 by smiranda          #+#    #+#             */
+/*   Updated: 2024/08/11 15:31:02 by smiranda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "builtin.h"
+
+void free_node(t_list **ptr)
+{
+    if (*ptr)
+    {
+        free((*ptr)->content);
+        (*ptr)->content = NULL;
+        free(*ptr);
+        *ptr = NULL;
+    }
+}
+
+void delete_env(char *argv, t_mini *mini) 
+{
+    t_list *env;
+    t_list *prev;
+    char *env_var;
+
+    env = mini->env_list;
+    prev = NULL;
+    while (env) 
+    {
+        env_var = (char *)env->content;
+        if (!strncmp(env_var, argv, ft_strlen(argv)) && env_var[ft_strlen(argv)] == '=')
+        {
+            if (prev) 
+                prev->next = env->next;  // Link previous node to the next node
+            else
+                mini->env_list = env->next;  // Update head if deleting the first node
+            free_node(&env);
+            return;
+        }
+        prev = env;
+        env = env->next;
+    }
+}
+
+int builtin_unset(char **argv)
+{
+    int exit_code;
+    t_mini *mini;
+
+    exit_code = EXIT_SUCCESS;
+    argv++;
+    mini = mini();
+    if (*argv && is_option(*argv, "unset"))
+        return(EXIT_FAILURE);
+    while (*argv)
+    {
+        if (!search_env(*argv) || ft_strchr(*argv, '='))
+        {
+            print_error("unset", *argv, "environmental variable not found");
+            exit_code = EXIT_FAILURE;
+        }
+        else
+           delete_env(*argv, mini);
+        argv++; 
+    }
+    return(exit_code);
+}
