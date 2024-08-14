e8533662fe1176c3effb0c71b2b501603da77385
Siria Lizeth Miranda Rendon <smiranda@2-A-5.42heilbronn.de>
2024-08-14 18:07:37 +0200

    edge case handled

--- src/builtin/builtin_export.c
+++ src/builtin/builtin_export.c
@@ -6,85 +6,110 @@
 /*   By: smiranda <smiranda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 19:31:17 by smiranda          #+#    #+#             */
-/*   Updated: 2024/08/13 14:25:48 by smiranda         ###   ########.fr       */
+/*   Updated: 2024/08/14 18:06:25 by smiranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-static int handle_export(char *argv)
-{
-    int flag_em;
+/**
+ * @brief Prints a list of environment variables.
+ */
 
-    if (ft_strchr(argv, '='))
-        flag_em = ft_add_env(argv);
-    else
-        flag_em = ft_empty_env(&argv);
-    if (flag_em == EXIT_FAILURE)
-        return (EXIT_FAILURE);
-    return (EXIT_SUCCESS);
+static int	print_list(t_list *list)
+{
+	t_list	*ptr;
+
+	ptr = list;
+	while (ptr)
+	{
+		ft_putendl_fd(ptr->content, STDOUT_FILENO);
+		ptr = ptr->next;
+	}
+	ft_lstclear(&list, free);
+	return (EXIT_SUCCESS);
+}
+/**
+ * @brief Copies the environment variables.
+ */
+
+static t_list	*copy_list(t_list *list)
+{
+	t_list	*copy;
+	t_list	*new;
+
+	copy = NULL;
+	while (list)
+	{
+		new = ft_lstnew(ft_strdup(list->content));
+		if (!new)
+		{
+			ft_lstclear(&copy, free);
+			return (NULL);
+		}
+		ft_lstadd_back(&copy, new);
+		list = list->next;
+	}
+	return (copy);
 }
 
-static int print_list(t_list *list)
-{
-    t_list *ptr;
+/**
+ * @brief Prints a sorted copy environment variables.
+ */
 
-    ptr = list;
-    while (ptr)
-    {
-        ft_putendl_fd(ptr->content, STDOUT_FILENO);
-        ptr = ptr->next;
-    }
-    ft_lstclear(&list, free);
-    return (EXIT_SUCCESS);
+static int	print_sort_env(void)
+{
+	t_mini	*mini;
+	t_list	*copy;
+
+	mini = get_mini();
+	copy = copy_list(mini->env_list);
+	if (!copy)
+		return (EXIT_FAILURE);
+	return (print_list(ft_lstsort(copy)));
 }
 
-static t_list *copy_list(t_list *list)
-{
-    t_list *copy;
-    t_list *new;
+/**
+ * @brief Adds or updates environment variables.
+ *  - If argument contains '=',
+	adds or updates environment variable with name and value.
+ *  - If argument does not contain '=',
+	adds environment variable with empty string as value.
+ */
 
-    copy = NULL;
-    while (list)
-    {
-        new = ft_lstnew(ft_strdup(list->content));
-        if (!new)
-        {
-            ft_lstclear(&copy, free);
-            return (NULL);
-        }
-        ft_lstadd_back(&copy, new);
-        list = list->next;
-    }
-    return (copy);
+static int	handle_export(char *argv)
+{
+	int	flag_em;
+
+	if (ft_strchr(argv, '='))
+		flag_em = ft_add_env(argv);
+	else
+		flag_em = ft_empty_env(&argv);
+	if (flag_em == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
 
-static int print_sort_env(void)
+/**
+ * @brief Handles export builtin command.
+ * - If no arguments, prints sorted environment variables.
+ * - If arguments, adds or updates environment variables.
+ */
+
+int	builtin_export(char **argv)
 {
-    t_mini *mini;
-    t_list *copy;
+	int	i;
 
-    mini = get_mini();
-    copy = copy_list(mini->env_list);
-    if (!copy)
-        return (EXIT_FAILURE);
-    return (print_list(ft_lstsort(copy)));
-}
-
-int builtin_export(char **argv)
-{
-    int i;
-
-    i = 1;
-    if (!argv[i])
-        return(print_sort_env());
-    if (is_option(argv[i], "export"))
-        return(EXIT_FAILURE);
-    while (argv[i])
-    {
-        if (handle_export(argv[i]) == EXIT_FAILURE)
-            return(EXIT_FAILURE);
-        i++;
-    }
-    return(EXIT_SUCCESS);  
+	i = 1;
+	if (!argv[i])
+		return (print_sort_env());
+	if (is_option(argv[i], "export"))
+		return (EXIT_FAILURE);
+	while (argv[i])
+	{
+		if (handle_export(argv[i]) == EXIT_FAILURE)
+			return (EXIT_FAILURE);
+		i++;
+	}
+	return (EXIT_SUCCESS);
 }
--- src/builtin/builtin_export_utils.c
+++ src/builtin/builtin_export_utils.c
@@ -6,102 +6,135 @@
 /*   By: smiranda <smiranda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 11:45:59 by smiranda          #+#    #+#             */
-/*   Updated: 2024/08/13 14:26:32 by smiranda         ###   ########.fr       */
+/*   Updated: 2024/08/14 18:02:47 by smiranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 
-t_list *ft_search_env(char *name)
-{
-    t_list *ptr;
-    char *env_var;
-    int lkey;
-    t_mini *mini;
+/**
+ * @brief Searches for an environment variable by name.
+ * - Takes the name of the environment variable.
+ * - Returns a pointer to the environment variable if found.
+ */
 
-    mini = get_mini();
-    lkey = ft_strlen(name);
-    if (!lkey)
-        return NULL;
-    ptr = mini->env_list;
-    while (ptr)
-    {
-        env_var = (char *)ptr->content;
-        if (!ft_strncmp(env_var, name, lkey) && env_var && env_var[lkey] == '=')
-            return (ptr);
-        ptr = ptr->next;
-    }
-    return (NULL);
+t_list	*ft_search_env(char *name)
+{
+	t_list	*ptr;
+	char	*env_var;
+	int		lkey;
+	t_mini	*mini;
+
+	mini = get_mini();
+	lkey = ft_strlen(name);
+	if (!lkey)
+		return (NULL);
+	ptr = mini->env_list;
+	while (ptr)
+	{
+		env_var = (char *)ptr->content;
+		if (!ft_strncmp(env_var, name, lkey) && env_var && env_var[lkey] == '=')
+			return (ptr);
+		ptr = ptr->next;
+	}
+	return (NULL);
+}
+/**
+ * @brief Updates the value of an environment variable.
+ * - Takes a pointer to the environment variable, the name and the new value.
+ * - Updates the value of the environment variable.
+ */
+
+static int	set_env_export(t_list *ptr, char *name, char *value)
+{
+	char	*new_env_var;
+	char	*temp;
+
+	temp = ft_strjoin(name, "=");
+	if (!temp)
+		return (EXIT_FAILURE);
+	new_env_var = ft_strjoin(temp, value);
+	free(temp);
+	if (!new_env_var)
+		return (EXIT_FAILURE);
+	free(ptr->content);
+	ptr->content = new_env_var;
+	return (EXIT_SUCCESS);
 }
 
-static int set_env_export(t_list *ptr, char *name, char *value)
+/**
+ * @brief Adds environment variables to the end
+ * with name and value extracted from argv.
+ */
+static int	put_env(char *name, char *value)
 {
-    char *new_env_var;
-    char *temp;
+	t_mini	*mini;
+	char	*new_env_var;
+	char	*temp;
 
-    temp = ft_strjoin(name, "=");
-    if (!temp)
-        return (EXIT_FAILURE);
-    new_env_var = ft_strjoin(temp, value);
-    free(temp);
-    if (!new_env_var)
-        return (EXIT_FAILURE);
-    free(ptr->content);
-    ptr->content = new_env_var;
-    return (EXIT_SUCCESS);
-}
-static int put_env(char *name, char* value)
-{
-    t_mini *mini;
-    char *new_env_var;
-    char *temp;
-
-    mini = get_mini();
-    temp = ft_strjoin(name, "=");
-    if (!temp)
-        return (EXIT_FAILURE);
-    new_env_var = ft_strjoin(temp, value);
-    free(temp);
-    if (!new_env_var)
-        return (EXIT_FAILURE);
-    ft_lstadd_back(&(mini->env_list), ft_lstnew(new_env_var));
-    return (EXIT_SUCCESS);
+	mini = get_mini();
+	temp = ft_strjoin(name, "=");
+	if (!temp)
+		return (EXIT_FAILURE);
+	new_env_var = ft_strjoin(temp, value);
+	free(temp);
+	if (!new_env_var)
+		return (EXIT_FAILURE);
+	ft_lstadd_back(&(mini->env_list), ft_lstnew(new_env_var));
+	return (EXIT_SUCCESS);
 }
 
-int ft_empty_env(char **argv)
-{
-    t_list *env_entry; //change improv
-    int add_empty_env;
+/**
+ * @brief Adds environment variables with empty value.
+ * - Takes from argv the name of the environment variable.
+ * - If the name is not an environment variable,
+	adds it to the list with empty string as value.
+ */
 
-    env_entry = ft_search_env(*argv);
-    if (!env_entry)
-    {
-        add_empty_env = put_env(*argv, "");
-        if (add_empty_env == EXIT_FAILURE)
-            return (EXIT_FAILURE);
-    }
-    return (EXIT_SUCCESS);
+int	ft_empty_env(char **argv)
+{
+	int		add_empty_env;
+	t_list	*env_entry;
+
+	env_entry = ft_search_env(*argv);
+	if (!env_entry)
+	{
+		add_empty_env = put_env(*argv, "\'\'");
+		if (add_empty_env == EXIT_FAILURE)
+			return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
 }
 
-int ft_add_env(char *argv)
-{
-    char *name;
-    char *value;
-    t_list *env_entry;
-    int flag;
+/**
+ * @brief Adds or updates environment variables with name and value.
+ * - Takes from argv the name and value of the environment variable.
+ * - If the name is already an environment variable, updates the value.
+ * - If the name is not an environment variable, adds it to the list.
+ */
 
-    name = ft_substr(argv, 0, ft_strchr(argv, '=') - argv);
-    value = ft_strdup(ft_strchr(argv, '=') + 1);
-    if (!name || !value)
-            return (EXIT_FAILURE);
-    env_entry = ft_search_env(name);
-    if (env_entry) //check if the name is already an env var
-        flag = set_env_export(env_entry, name, value); //add the new value to the env var
-    else
-        flag = put_env(name, value); //if the name is not an env var, add it to the list
-    free(name);
-    free(value);
-    if (flag == EXIT_FAILURE)
-        return (EXIT_FAILURE);
-    return (EXIT_SUCCESS);
+int	ft_add_env(char *argv)
+{
+	char	*name;
+	char	*value;
+	t_list	*env_entry;
+	int		flag;
+
+	name = ft_substr(argv, 0, ft_strchr(argv, '=') - argv);
+	value = ft_strdup(ft_strchr(argv, '=') + 1);
+	if (!name || !value)
+		return (EXIT_FAILURE);
+	env_entry = ft_search_env(name);
+	if (env_entry)
+		// check if the name is already an env var
+		flag = set_env_export(env_entry, name, value);
+	// add the new value to the env var
+	else
+		flag = put_env(name, value); // if the name is not an env var,
+										// add it to the list
+	free(name);
+	free(value);
+	if (flag == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
